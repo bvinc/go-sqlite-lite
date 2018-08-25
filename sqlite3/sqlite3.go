b593653e509469c0cf039a6797ddc87f923048bb
Brian Vincent <brainn@gmail.com>
2018-08-25 16:41:37 -0500

    Added -lm to LDFLAGS for linux

--- sqlite3/sqlite3.go
+++ sqlite3/sqlite3.go
@@ -33,6 +33,7 @@ package sqlite3
 #cgo CFLAGS: -DSQLITE_THREADSAFE=2
 #cgo CFLAGS: -DSQLITE_USE_ALLOCA=1
 #cgo CFLAGS: -DSQLITE_USE_URI=1
+#cgo linux LDFLAGS: -lm
 
 #cgo linux,!android CFLAGS: -DHAVE_FDATASYNC=1
 #cgo linux,!android CFLAGS: -DHAVE_PREAD64=1 -DHAVE_PWRITE64=1
